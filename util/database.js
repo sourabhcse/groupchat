757cb3a013cb1854faaf778f7c7506a0c20b37a3
sourabhcse <sourabhsaini201098@gmail.com>
2023-02-10 22:18:37 +0530

    Create database.js

---
+++ util/database.js
@@ -0,0 +1,6 @@
+const Sequelize=require('sequelize')
+const sequelize = new Sequelize("groupchat" , "root","210431@rath" ,{
+    dialect: "mysql",
+    host: localhost
+  });
+  module.exports = sequelize;
