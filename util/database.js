d9dd1632d1536c69cc9e3f40aaca9f3758ab7bb8
sourabhcse <sourabhsaini201098@gmail.com>
2023-02-11 11:49:29 +0530

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize=require('sequelize')
 const sequelize = new Sequelize("groupchat" , "root","210431@rath" ,{
     dialect: "mysql",
-    host: localhost
+    host: "localhost"
   });
   module.exports = sequelize;
